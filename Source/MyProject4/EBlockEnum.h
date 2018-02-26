94728f17041b4179edab599d11fba1e01586f71d
UlisesCeca <ulises@ulisesceca.com>
2018-02-26 14:26:46 +0100

    added enum for blocks

---
+++ Source/MyProject4/EBlockEnum.h
@@ -0,0 +1,8 @@
+UENUM()		//"BlueprintType" is essential to include
+enum class EBlockEnum : uint8
+{
+	BE_Down 	UMETA(DisplayName = "DOWN"),
+	BE_Up 		UMETA(DisplayName = "UP"),
+	BE_Right 	UMETA(DisplayName = "RIGHT"),
+	BE_Left		UMETA(DisplayName = "LEFT")
+};
