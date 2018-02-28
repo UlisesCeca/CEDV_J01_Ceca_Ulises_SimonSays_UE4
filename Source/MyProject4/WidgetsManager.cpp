b1474388a7083370372de136f779c88c3db6f061
UlisesCeca <ulises@ulisesceca.com>
2018-02-28 07:57:08 +0100

    working records with a cool widget

---
+++ Source/MyProject4/WidgetsManager.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "WidgetsManager.h"
+
+
+// Sets default values
+AWidgetsManager::AWidgetsManager()
+{
+
+}
+
---
+++ Source/MyProject4/WidgetsManager.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "WidgetsManager.generated.h"
+
+UCLASS()
+class MYPROJECT4_API AWidgetsManager : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AWidgetsManager();	
+	
+};
